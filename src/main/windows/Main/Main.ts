4dab891ecdad9691f65b56bf1df0bbb9a9db7db6
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2020-12-05 15:37:21 -0800

    refactor: create main window class

---
+++ src/main/windows/Main/Main.ts
@@ -0,0 +1,32 @@
+import { BrowserWindow } from 'electron'
+import * as path from 'path'
+import * as url from 'url'
+
+import { isProd } from '@main/utils'
+
+export class MainWindow extends BrowserWindow {
+  constructor() {
+    super({
+      height: 600,
+      width: isProd ? 500 : 800,
+      icon: `${__dirname}/public/icons/Icon_256x256.png`,
+      webPreferences: {
+        nodeIntegration: true,
+        devTools: !isProd,
+      },
+    })
+
+    // load the index.html of the app.
+    this.loadURL(
+      url.format({
+        pathname: path.join(__dirname, './index.html'),
+        protocol: 'file:',
+        slashes: true,
+      }),
+    )
+
+    if (!isProd) {
+      this.webContents.openDevTools()
+    }
+  }
+}
