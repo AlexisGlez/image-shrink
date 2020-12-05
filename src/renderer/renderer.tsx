ae466e6a6d7d2663ed25a7a6a83a31f0d5b8c81a
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2020-12-05 14:24:01 -0800

    feat: add materialize and font awesome dependencies

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,16 +1,15 @@
 /**
  * React renderer.
  */
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-// Import the styles here to process them with webpack
-import '@public/style.css';
+import '@public/style.css'
 
 ReactDOM.render(
-  <div className='app'>
+  <div className="app">
     <h4>Welcome to React, Electron and Typescript</h4>
     <p>Hello</p>
   </div>,
-  document.getElementById('app')
-);
+  document.getElementById('app'),
+)
