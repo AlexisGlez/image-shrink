7488a177937f7897980032cb36253fd6f65bdd31
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2020-12-05 15:25:44 -0800

    feat: create main window (#3)

---
+++ src/renderer/screens/Home/Home.tsx
@@ -0,0 +1,85 @@
+const path = require('path')
+const os = require('os')
+const { ipcRenderer } = require('electron')
+
+import * as React from 'react'
+
+import './Home.css'
+
+export const Home: React.FC<{}> = () => {
+  const imageField = React.useRef<HTMLInputElement>(null)
+  const sliderField = React.useRef<HTMLInputElement>(null)
+
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!imageField.current || !imageField.current.files || !imageField.current.files[0]) {
+      return
+    }
+
+    const imgPath = imageField.current.files[0].path
+
+    if (!sliderField.current) {
+      return
+    }
+
+    const quality = sliderField.current.value
+
+    console.log(imgPath, quality)
+
+    ipcRenderer.send('image:minimize', {
+      imgPath,
+      quality,
+    })
+  }
+
+  ipcRenderer.on('image:done', () => {
+    if (!sliderField.current) {
+      return
+    }
+
+    // @ts-ignore: "M" is coming from materialize global variable
+    M.toast({
+      html: `Image resized to ${sliderField.current.value}% quality`,
+    })
+  })
+
+  return (
+    <div className="container center-align">
+      <h3>
+        <i className="fas fa-images"></i> ImageShrink
+      </h3>
+      <p>Choose an image to resize</p>
+
+      <form id="image-form" onSubmit={onFormSubmit}>
+        <div className="file-field input-field">
+          <div className="w-full">
+            <span>Browse</span>
+            <input type="file" id="img" ref={imageField} />
+          </div>
+
+          <div className="file-path-wrapper">
+            <input type="text" className="file-path validate" placeholder="Upload file" />
+          </div>
+        </div>
+
+        <p>
+          <strong>Quality:</strong>
+          <em>The lower the quality, the smaller the file size</em>
+        </p>
+
+        <p className="range-field">
+          <input type="range" min="0" max="100" id="slider" ref={sliderField} />
+        </p>
+
+        <input type="submit" value="Resize" className="w-full black" />
+      </form>
+
+      <div className="card output">
+        <div className="card-content">
+          Output Path: <span id="output-path">{path.join(os.homedir(), 'image-shrink')}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const lodash = require('lodash');
-const CopyPkgJsonPlugin = require('copy-pkg-json-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const lodash = require('lodash')
+const CopyPkgJsonPlugin = require('copy-pkg-json-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 function srcPaths(src) {
-  return path.join(__dirname, src);
+  return path.join(__dirname, src)
 }
 
-const isEnvProduction = process.env.NODE_ENV === 'production';
-const isEnvDevelopment = process.env.NODE_ENV === 'development';
+const isEnvProduction = process.env.NODE_ENV === 'production'
+const isEnvDevelopment = process.env.NODE_ENV === 'development'
 
 // #region Common settings
 const commonConfig = {
@@ -45,15 +45,22 @@ const commonConfig = {
           name: '[path][name].[ext]',
         },
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
     ],
   },
-};
+}
 // #endregion
 
-const mainConfig = lodash.cloneDeep(commonConfig);
-mainConfig.entry = './src/main/main.ts';
-mainConfig.target = 'electron-main';
-mainConfig.output.filename = 'main.bundle.js';
+const mainConfig = lodash.cloneDeep(commonConfig)
+mainConfig.entry = './src/main/main.ts'
+mainConfig.target = 'electron-main'
+mainConfig.output.filename = 'main.bundle.js'
 mainConfig.plugins = [
   new CopyPkgJsonPlugin({
     remove: ['scripts', 'devDependencies', 'build'],
@@ -63,16 +70,16 @@ mainConfig.plugins = [
       postinstall: 'electron-builder install-app-deps',
     },
   }),
-];
+]
 
-const rendererConfig = lodash.cloneDeep(commonConfig);
-rendererConfig.entry = './src/renderer/renderer.tsx';
-rendererConfig.target = 'electron-renderer';
-rendererConfig.output.filename = 'renderer.bundle.js';
+const rendererConfig = lodash.cloneDeep(commonConfig)
+rendererConfig.entry = './src/renderer/renderer.tsx'
+rendererConfig.target = 'electron-renderer'
+rendererConfig.output.filename = 'renderer.bundle.js'
 rendererConfig.plugins = [
   new HtmlWebpackPlugin({
     template: path.resolve(__dirname, './public/index.html'),
   }),
-];
+]
 
-module.exports = [mainConfig, rendererConfig];
+module.exports = [mainConfig, rendererConfig]
